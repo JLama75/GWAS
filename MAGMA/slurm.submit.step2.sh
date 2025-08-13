8243573815502720acc507962308355ecc65905d
Jyoti Lama <97466232+JLama75@users.noreply.github.com>
2025-08-13 13:19:55 -0400

    Update slurm.submit.step2.sh

--- MAGMA/slurm.submit.step2.sh
+++ MAGMA/slurm.submit.step2.sh
@@ -4,6 +4,6 @@ Trait="Your_Study_Name"
 echo Trait: $Trait
 
 for j in 'GO_BP' 'GO_CC' 'GO_MF' 'HALLMARK' 'KEGG' 'MGI' 'REACTOME'; do
-  sbatch sc_magma_GeneSet.step2.sh $Trait $j
+  sbatch sc_magma_GeneSet.STEP2.sh $Trait $j
 done
 
